32095e28b460e508e904dce53fb010a110827748
zio_tom78 <zio_tom78@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-02-21 12:17:58 +0000

    License and short comment added at the beginning of the source file.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rfitsio@32 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/fits_files/create-fits.c
+++ pkg/inst/fits_files/create-fits.c
@@ -1,3 +1,30 @@
+/* -*- mode: c -*- */
+
+/* Compile this program with
+ *
+ *   cc -o create-fits create-fits.c -lcfitsio -lm
+ */
+
+/* C program to create a number of FITS files useful for testing
+ * RFITSIO.
+ *
+ * Copyright (C) 2011 Maurizio Tomasi
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 #include <fitsio.h>
 #include <stdio.h>
 
